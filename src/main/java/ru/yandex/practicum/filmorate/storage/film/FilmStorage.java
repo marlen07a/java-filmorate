ded52dd95733b20bda7f330fa470f508bff62fca
viktoravramenko <viktoravramenkl@yandex.com>
2025-09-28 21:26:06 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,14 +1,21 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     List<Film> findAll();
+
     Film create(Film film);
+
     Film update(Film film);
+
     Optional<Film> findById(Long id);
+
     void delete(Long id);
+
     boolean existsById(Long id);
 }
