49c389713bda56ce96dcc0b35f597243a54accd2
viktoravramenko <viktoravramenkl@yandex.com>
2025-10-18 21:48:46 +0300

    checkstyle2

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -1,14 +1,20 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
 import ru.yandex.practicum.filmorate.model.Review;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface ReviewStorage {
     Review create(Review review);
+
     Review update(Review review);
+
     void delete(Long reviewId);
+
     Optional<Review> findById(Long reviewId);
+
     List<Review> findByFilmId(Long filmId, int count);
+
     List<Review> findAll(int count);
 }
