d47f272696e20fa85e8b9e30d11cfa6c457f9780
SemenTeplov <eburg-severca@mail.ru>
2025-10-24 20:22:42 +0500

    add-develop: исправление.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -413,7 +413,8 @@ public class FilmDbStorage implements FilmStorage {
 //            default -> throw new IllegalArgumentException("Invalid sort parameter: " + sortBy);
 //        }
 
-        String sql = "SELECT f.id, f.name, f.description, f.release_date, f.duration, f.created_at, mpa_id, mpa_name, mpa_description" +
+        String sql = "SELECT f.id, f.name, f.description, f.release_date, f.duration, f.created_at, m.id AS mpa_id, " +
+                "m.name AS mpa_name, m.description AS mpa_description" +
                 "FROM films f LEFT JOIN mpa_ratings m ON f.mpa_id = m.id";
 
 //        String sql = String.format("""
