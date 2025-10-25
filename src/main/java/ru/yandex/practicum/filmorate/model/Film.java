1cf5ea2314e4e6dca89918f1766b6ad652b3dac7
SemenTeplov <eburg-severca@mail.ru>
2025-10-25 21:54:18 +0500

    add-marks: Работа с ревью.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 @Data
 public class Film {
@@ -29,7 +30,7 @@ public class Film {
     private LocalDateTime createdAt;
     private Set<Long> likes = new HashSet<>();
     private MPA mpa;
-    private Set<Genre> genres = new HashSet<>();
+    private Set<Genre> genres = new TreeSet<>();
     private Set<Director> directors = new HashSet<>();
 
     public static final LocalDate RULE_FILM_DATE = LocalDate.of(1895, 12, 28);
