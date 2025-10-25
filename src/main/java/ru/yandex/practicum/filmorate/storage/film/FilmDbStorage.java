3ccee238af4cff4fa8fa55b9fa38bb723672d197
SemenTeplov <eburg-severca@mail.ru>
2025-10-25 18:17:01 +0500

    add-develop: исправление.

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -91,7 +91,9 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public List<Film> getPopularFilms(int count) {
-        return jdbcTemplate.query(FIND_ALL_SQL_WITH_LIKES_COUNT + " " + ORDER_BY_LIKES + count, this::mapRowToFilm);
+        String sql = FIND_ALL_SQL_WITH_LIKES_COUNT + " " + ORDER_BY_LIKES + count;
+
+        return jdbcTemplate.query(sql, this::mapRowToFilm);
     }
 
     @Override
