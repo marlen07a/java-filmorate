4b3bde1d6e602954c7a7b3d0b926bff3ccad99a7
SemenTeplov <eburg-severca@mail.ru>
2025-10-25 16:23:49 +0500

    add-develop: исправление.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -12,7 +12,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static ru.yandex.practicum.filmorate.model.Film.RULE_FILM_DATE;
 
