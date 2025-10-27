fdd51fd56a40b08875d96b97498fd43faf67e84f
SemenTeplov <eburg-severca@mail.ru>
2025-10-27 19:33:38 +0500

    add-marks: добавление оценки

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -99,8 +99,13 @@ public class FilmDbStorage implements FilmStorage {
 //                .toList();
 
         return jdbcTemplate.query("SELECT *, m.id AS mpa_id, m.name AS mpa_name, m.description AS mpa_description, " +
-                "COUNT(fl.user_id) AS count_likes FROM films AS f LEFT JOIN mpa_ratings AS m ON f.mpa_id = m.id " +
-                "LEFT JOIN film_likes AS fl ON f.id = fl.film_id GROUP BY f.id ORDER BY count_likes DESC LIMIT " + count,
+                "COUNT(fl.user_id) AS count_likes " +
+                "FROM films " +
+                "LEFT JOIN mpa_ratings m ON films.mpa_id = m.id " +
+                "LEFT JOIN film_likes fl ON films.id = fl.film_id " +
+                "GROUP BY films.id " +
+                "ORDER BY count_likes DESC " +
+                "LIMIT " + count,
                 this::mapRowToFilm);
     }
 
